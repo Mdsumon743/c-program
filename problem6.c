ba1309d30f607d88be62fdf63f3ee23d396a59a1
myusername <mdsumonhossan743@gmail.com>
2025-08-03 22:28:54 +0600

    ...

---
+++ problem6.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+
+    // max
+    int max = 0;
+    if (a < b && c < b)
+    {
+        max = b;
+    }
+    else if (b < a && c < a)
+    {
+        max = a;
+    }
+    else
+    {
+        max = c;
+    }
+    //min 
+    int  min = 0;
+    if(b>a && c>a){
+      min = a;
+    }else if (a>b && c>b){
+        min = b;
+    }else{
+        min = c;
+    }
+    printf("%d% d",min ,max);
+    return 0;
+}
